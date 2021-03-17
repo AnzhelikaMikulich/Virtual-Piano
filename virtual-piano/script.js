18518f08c27434761ea48309cdf0fa80bc5c0d43
AnzhelikaMikulich <angelika.flyorko@gmail.com>
2021-03-18 02:38:52 +0300

    refactor: change structure of the project and add style css

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -2,6 +2,7 @@ const fullscreen = document.querySelector('.openfullscreen');
 const notes = document.querySelector('.btn-notes');
 const letters = document.querySelector('.btn-letters');
 const pianoKey = document.querySelectorAll('.piano-key');
+const piano = document.querySelector('.piano');
 
 fullscreen.addEventListener('click', changeScreen);
 
@@ -32,29 +33,26 @@ function changeLetters() {
 	}
 }
 
-pianoKey.forEach((key) => {
-	key.addEventListener('click', playNote);
-});
+
+piano.addEventListener('mousedown', playNote);
 
 function playNote(event) {
 	let key = event.target;
-	key.classList.add('piano-key-active');
+	key.classList.add('piano-key-active','piano-key-active-pseudo');
 	let note = document.getElementById(key.dataset.note);
 	note.currentTime = 0;
 	note.play();
-    note.addEventListener('ended', () => {
-        key.classList.remove('piano-key-active');
-    });
+	key.addEventListener('mouseup',() => {
+		key.classList.remove('piano-key-active','piano-key-active-pseudo');
+	});
 }
 
 window.addEventListener('keydown', playNoteKeyboard);
 
 function playNoteKeyboard(e) {
-	console.log(e.keyCode);
 	let note = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 	let key = document.querySelector(`.piano-key[data-key="${e.keyCode}"]`);
 	if (!key) return;
-	console.log(key);
 	key.classList.add('piano-key-active');
     if(e.repeat) return true;
 	note.currentTime = 0;
@@ -62,21 +60,22 @@ function playNoteKeyboard(e) {
 	note.addEventListener('ended', () => {
 		key.classList.remove('piano-key-active');
 	});
+	
 }
 
-pianoKey.forEach((key) => {
-	key.addEventListener('mouseover', playNoteMovingMouse);
-});
+
+piano.addEventListener('mouseover', playNoteMovingMouse);
+
 
 function playNoteMovingMouse(event) {
-	if (event.which == 1&& event.target.classList.contains('piano-key')) {
+	 if (event.which == 1&& event.target.classList.contains('piano-key')) {
 		let key = event.target;
-		key.classList.add('piano-key-active');
+		key.classList.add('piano-key-active','piano-key-active-pseudo');
 		let note = document.getElementById(key.dataset.note);
 		note.currentTime = 0;
 		note.play();
-		note.addEventListener('ended', () => {
-			key.classList.remove('piano-key-active');
+		key.addEventListener('mouseout', () => {
+			key.classList.remove('piano-key-active','piano-key-active-pseudo');
 		});
 	}
 }
